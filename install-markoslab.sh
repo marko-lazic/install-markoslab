6be544716e45229c162d85a36d30fdf0e8f711a6
Marko <zila015@hotmail.com>
2015-03-26 21:37:24 +0100

    cleared script

--- install-markoslab.sh
+++ install-markoslab.sh
@@ -1,45 +1,46 @@
 #!/bin/sh
 echo "adding ppa's..."
-sudo add-apt-repository -y ppa:danielrichter2007/grub-customizer > /dev/null 2>&1
-sudo add-apt-repository -y ppa:dimula73/krita > /dev/null 2>&1
-sudo add-apt-repository -y ppa:inkscape.dev/stable > /dev/null 2>&1
-sudo add-apt-repository -y ppa:kilian/f.lux > /dev/null 2>&1
-sudo add-apt-repository -y ppa:maarten-baert/simplescreenrecorder > /dev/null 2>&1
-sudo add-apt-repository -y ppa:mixxx/mixxx > /dev/null 2>&1
-sudo add-apt-repository -y ppa:caffeine-developers/ppa > /dev/null 2>&1
-sudo add-apt-repository -y ppa:noobslab/themes > /dev/null 2>&1
-sudo add-apt-repository -y ppa:numix/ppa > /dev/null 2>&1
-sudo add-apt-repository -y ppa:paolorotolo/android-studio > /dev/null 2>&1
-sudo add-apt-repository -y ppa:pinta-maintainers/pinta-stable > /dev/null 2>&1
-sudo add-apt-repository -y ppa:thomas-schiex/blender > /dev/null 2>&1
-sudo add-apt-repository -y ppa:ubuntu-wine/ppa > /dev/null 2>&1
-sudo add-apt-repository -y ppa:ubuntu-sdk-team/ppa > /dev/null 2>&1
-sudo add-apt-repository -y ppa:xorg-edgers/ppa > /dev/null 2>&1
-sudo add-apt-repository -y ppa:videolan/stable-daily > /dev/null 2>&1
-sudo add-apt-repository -y ppa:audacity-team/daily > /dev/null 2>&1
-sudo add-apt-repository -y ppa:kxstudio-team/ppa > /dev/null 2>&1
-sudo add-apt-repository -y ppa:vikoadi/ppa > /dev/null 2>&1
-sudo add-apt-repository -y ppa:nilarimogard/webupd8 > /dev/null 2>&1
-sudo add-apt-repository -y ppa:gnome3-team/gnome3-staging > /dev/null 2>&1
+sudo add-apt-repository -y ppa:danielrichter2007/grub-customizer
+sudo add-apt-repository -y ppa:dimula73/krita
+sudo add-apt-repository -y ppa:inkscape.dev/stable
+sudo add-apt-repository -y ppa:kilian/f.lux
+sudo add-apt-repository -y ppa:maarten-baert/simplescreenrecorder
+sudo add-apt-repository -y ppa:mixxx/mixxx
+sudo add-apt-repository -y ppa:caffeine-developers/ppa
+sudo add-apt-repository -y ppa:noobslab/themes
+sudo add-apt-repository -y ppa:numix/ppa
+sudo add-apt-repository -y ppa:paolorotolo/android-studio
+sudo add-apt-repository -y ppa:pinta-maintainers/pinta-stable
+sudo add-apt-repository -y ppa:thomas-schiex/blender
+sudo add-apt-repository -y ppa:ubuntu-wine/ppa
+sudo add-apt-repository -y ppa:ubuntu-sdk-team/ppa
+sudo add-apt-repository -y ppa:xorg-edgers/ppa
+sudo add-apt-repository -y ppa:videolan/stable-daily
+sudo add-apt-repository -y ppa:audacity-team/daily
+sudo add-apt-repository -y ppa:kxstudio-team/ppa
+sudo add-apt-repository -y ppa:vikoadi/ppa
+sudo add-apt-repository -y ppa:nilarimogard/webupd8
+sudo add-apt-repository -y ppa:gnome3-team/gnome3-staging
 sudo add-apt-repository -y ppa:ubuntuhandbook1/corebird
 sudo add-apt-repository -y ppa:tomahawk/ppa
 sudo add-apt-repository -y ppa:yorba/daily-builds
 
 # google chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - > /dev/null 2>&1
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list' > /dev/null 2>&1
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 
 
 echo "updating..."
-sudo apt-get update > /dev/null 2>&1
+sudo apt-get update
 
 LIST_OF_APPS="grub-customizer simplescreenrecorder fluxgui inkscape  krita-2.9 mixxx caffeine numix-icon-theme-circle numix-icon-theme numix-gtk-theme android-studio pinta blender wine1.7 android-tools-adb android-tools-fastboot phablet-tools ubuntu-sdk nvidia-346 vlc google-chrome-stable audacity ardour3 steam indicator-kdeconnect kdeconnect jackd2 patchage gnome-subtitles youtube-dl haroopad git corebird tomahawk geary"
 
-LIBRARIES_FOR_STEAM="libgtk2.0-0:i386 libnss3-1d:i386 libnspr4-0d:i386 lib32nss-mdns libxml2:i386 libxslt1.1:i386"
+LIBRARIES_FOR_STEAM=" libgtk2.0-0:i386 libnss3-1d:i386 libnspr4-0d:i386 lib32nss-mdns libxml2:i386 libxslt1.1:i386"
 
 echo "installing apps..."
-sudo apt install -y $LIST_OF_APPS
+sudo apt install -y $LIST_OF_APPS $LIBRARIES_FOR_STEAM
 
 echo "done."
 
 
+#  > /dev/null 2>&1
