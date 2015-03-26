ce8dbb0fbda6f3d152e86588c27882b285e29a52
Marko <zila015@hotmail.com>
2015-03-26 01:17:54 +0100

    added wine installs

---
+++ install-wine.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo add-apt-repository -y ppa:ubuntu-wine/ppa     
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get -y install wine1.7 winetricks
+
+
+# Photoshop
+# https://kickass.to/adobe-photoshop-cs6-13-1-2-extended-repack-p2p-t7744263.html
