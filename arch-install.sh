32ad61c5703bbfe3a6def8b2106a5107689a24b4
Marko <zila015@hotmail.com>
2015-03-30 19:47:54 +0200

    Added arch script

---
+++ arch-install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# First need to enable multilib repository for 32bit libraries
+LIST_OF_STEAM="lib32-gtk2 lib32-sdl lib32-curl"
---
+++ ubuntu-wine.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo add-apt-repository -y ppa:ubuntu-wine/ppa     
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install -y wine1.7 winetricks
+
+
+# Photoshop
+# https://mega.co.nz/#!qoYFBLba!7WnBPjAD8VceRp4nNHi2VuAq9i72L8ngA7y5rWZ02rY
