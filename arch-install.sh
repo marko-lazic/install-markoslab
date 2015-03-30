1c10bbfaec33dbb7c595e346d3108f29520afd92
Marko <zila015@hotmail.com>
2015-03-30 20:27:30 +0200

    edited arch-insall

--- arch-install.sh
+++ arch-install.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
 # First need to enable multilib repository for 32bit libraries
-LIST_OF_STEAM="lib32-gtk2 lib32-sdl lib32-curl"
+
+LIST_OF_STEAM="lib32-gtk2 lib32-sdl lib32-curl lib32-gnutls"
+
+LIST_OF_NIVIDA="nvidia-340xx nvidia-340xx-libgl lib32-nvidia-304xx"
+
+LIST_OF_DESIGN="inkscape calligra-krita pinta gimp"
+
+LIST_OF_AUDIO="jack2 audacity patchage ardour"
+
+LIST_OF_DEV="android-tools"
+
+LIST_OF_VIDEO="vlc openshot simplescreenrecorder"
+
+LIST_OF_TEXT="gnome-subtitles "
