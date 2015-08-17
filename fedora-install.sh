6c3c6368b87866794e8f0853c5bc4250b2fcfb18
Marko <zila015@gmail.com>
2015-08-17 23:55:08 +0200

    updated fedora packages and cleaned scripts folder.

--- fedora-install.sh
+++ fedora-install.sh
@@ -1,5 +1,14 @@
 #!bin/sh
 
-#Oracle Virtual Box
-sudo dnf install virtualbox-4.2 dkms kernel-devel gcc
+
+sudo dnf copr enable jgillich/brackets helber/atom timlau/yumex-dnf piqus/gnome-builder leigh123linux/libreoffice gnumdk/lollypop
+LIST_OF_APPS="VirtualBox-5.0 dkms kernel-devel gcc audacity-freeworld mixxx jack-audio-connection-kit-dbus transmission lollypop libreoffice blender gnome-builder gimp gpick calligra-krita inkscape corebird filezila gparted easytag vsftpd polari converseen atom brackets pitivi kolourpaint unetbootin nmap-frontend yumex-dnf wine"
+sudo dnf update -y
+sudo dnf install -y LIST_OF_APPS
+
+#Oracle Virtual Box (modules for kernel)
 echo export KERN_DIR=/usr/src/kernels/`uname -r` >> ~/.bashrc
+
+# FTP aka. vsftpd configuration: vim /etc/vsftpd/vsftpd.conf
+firewall-cmd --permanent --add-port=21/tcp
+firewall-cmd --reload
