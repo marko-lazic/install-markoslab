79a393696cadace6b9dab1e764b45d1b4fc9f93b
Marko <zila015@gmail.com>
2016-02-19 02:01:57 +0100

    added pulseaudio-module-zeroconf paprefs to fedora and ubuntu

--- fedora-install.sh
+++ fedora-install.sh
@@ -2,7 +2,7 @@
 
 
 sudo dnf copr enable jgillich/brackets helber/atom timlau/yumex-dnf piqus/gnome-builder leigh123linux/libreoffice gnumdk/lollypop
-LIST_OF_APPS="VirtualBox-5.0 dkms kernel-devel gcc audacity-freeworld mixxx jack-audio-connection-kit-dbus transmission lollypop libreoffice blender gnome-builder gimp gpick calligra-krita inkscape corebird filezila gparted easytag vsftpd polari converseen atom brackets pitivi kolourpaint unetbootin nmap-frontend yumex-dnf wine"
+LIST_OF_APPS="pulseaudio-module-zeroconf paprefs VirtualBox-5.0 dkms kernel-devel gcc audacity-freeworld mixxx jack-audio-connection-kit-dbus transmission lollypop libreoffice blender gnome-builder gimp gpick calligra-krita inkscape corebird filezila gparted easytag vsftpd polari converseen atom brackets pitivi kolourpaint unetbootin nmap-frontend yumex-dnf wine"
 sudo dnf update -y
 sudo dnf install -y LIST_OF_APPS
 
